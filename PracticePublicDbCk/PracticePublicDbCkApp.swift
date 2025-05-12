281786746ee0563405dac26f6b343485591b6aa5
Saputra <saputra.uta50@gmail.com>
2025-05-12 10:10:10 +0800

    Initial Commit

---
+++ PracticePublicDbCk/PracticePublicDbCkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticePublicDbCkApp.swift
+//  PracticePublicDbCk
+//
+//  Created by Saputra on 12/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PracticePublicDbCkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
